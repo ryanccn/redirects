7d4bb6ac6557ba07c901ce898fa0cd5fb3580e06
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2021-11-07 11:38:29 +0800

    single out plain text header

--- responses.ts
+++ responses.ts
@@ -1,9 +1,11 @@
+const plainTextHeader = { 'Content-Type': 'text/plain' };
+
 export const redirect = (url: string, latency?: number) => {
   return new Response(`Redirecting you to ${url}...`, {
     headers: {
       location: url,
       'X-Database-Latency': ((latency ?? 0) / 1000).toFixed(2) + 's',
-      'Content-Type': 'text/plain',
+      ...plainTextHeader,
     },
 
     status: 302,
@@ -13,27 +15,27 @@ export const redirect = (url: string, latency?: number) => {
 export const notFound = () => {
   return new Response('404 Not Found!', {
     status: 404,
-    headers: { 'Content-Type': 'text/plain' },
+    headers: plainTextHeader,
   });
 };
 
 export const notAllowed = () => {
   return new Response('405 Method Not Allowed!', {
     status: 405,
-    headers: { 'Content-Type': 'text/plain' },
+    headers: plainTextHeader,
   });
 };
 
 export const badRequest = () => {
   return new Response('400 Bad Request!', {
     status: 400,
-    headers: { 'Content-Type': 'text/plain' },
+    headers: plainTextHeader,
   });
 };
 
 export const unauthorized = () => {
   return new Response('401 Unauthorized!', {
     status: 401,
-    headers: { 'Content-Type': 'text/plain' },
+    headers: plainTextHeader,
   });
 };
